bdf02c4e377591ea3e9e89956e58daaa2b900118
Christian Bolstad <christian@bolstad.se>
2016-01-02 12:47:48 +0100

    renamed to getDateForMondayForWeek

--- src/GuessYear.php
+++ src/GuessYear.php
@@ -14,20 +14,103 @@ class GuessYear
 
     private $currentYear;
     private $currentWeek;
+    private $currentDate;
 
     function __construct()
     {
         $this->setWeek(date("W"));
         $this->setYear(date("Y"));
+        $this->setDate(date("Y-m-d"));
     }
 
+    // SETTERS
+
+    /**
+     * Set current year
+     *
+     * @param $year integer - Current year in four digits - as returned as date("Y") (e.g. '2016')
+     */
     function setYear( $year ) {
         $this->currentYear = $year;;
     }
 
+    /**
+     * Set current week
+     *
+     * @param $week integer - Current week number - as returned by date("W") (e.g. 53)
+     */
     function setWeek( $week ) {
         $this->currentWeek = $week;
     }
 
+    /**
+     * Set current date formated by ISO 8601 ( YYYY-MM-DD )
+     *
+     * @param $date - string, as retuned by date("Y-m-d")
+     */
+    function setDate( $date ) {
+        $this->currentDate = $date;
+    }
+
+
+    // GETTERS
+
+    /**
+     * Return current date formated by ISO 8601 ( YYYY-MM-DD )
+     *
+     * @return string, as retuned by date("Y-m-d")
+     */
+    function getDate() {
+        return $this->currentDate;
+    }
+
+    /**
+     * Return current year in for digits
+     *
+     * @return integer - as returned as date("Y") (e.g. '2016')
+     */
+    function getYear() {
+        return $this->currentYear;
+    }
+
+    /**
+     * Return current week
+     *
+     * @return integer -- Current week number - as returned by date("W") (e.g. 53)
+     */
+    function getWeek() {
+        return $this->currentWeek;
+    }
+
+    // Tricky week functions
+
+    /**
+     * Return the date of the monday of the current week
+     *
+     * @return string -  date formated by ISO 8601 ( YYYY-MM-DD )
+     */
+    function getDateForMondayForWeek() {
+        return date("Y-m-d",strtotime("last monday", strtotime( $this->getDate())));
+    }
+
+    /**
+     * Return the year of the monday of the currenty week
+     *
+     * @return string - year as returned by date("Y")
+     */
+    function getYearForMondayForWeek() {
+        return date("Y",strtotime($this->getDateForMondayForWeek()));
+    }
+
+
+    /**
+     * Return the week for the monday of the current week
+     *
+     *
+     * @return  integer -- Current week number - as returned by date("W") (e.g. 53)
+     */
+    function getWeekForMondayForWeek() {
+        return date("W",strtotime($this->getDateForMondayForWeek()));
+    }
 
 }
--- tests/GuessYearTest.php
+++ tests/GuessYearTest.php
@@ -14,6 +14,10 @@ class GuessYearTest extends \PHPUnit_Framework_TestCase
 
     public $guessYear;
 
+    private $test_date  = "2016-01-02";
+    private $test_year = 2016;
+    private $test_week = 53;
+
     /**
      * Setup common stuff for all tests
      */
@@ -22,11 +26,35 @@ class GuessYearTest extends \PHPUnit_Framework_TestCase
         // Since we are working with date stuff we *must* set a default timezone
         date_default_timezone_set("UTC");
         $this->guessYear = new \Bolstad\WeekYear\GuessYear();
-
+        $this->guessYear->setYear($this->test_year);
+        $this->guessYear->setWeek($this->test_week);
+        $this->guessYear->setDate($this->test_date);
     }
 
-    function testGetCurrentWeek()
+    function testGetDate() {
+        $this->assertEquals($this->test_date,$this->guessYear->getDate());
+    }
+
+    function testGetYear() {
+        $this->assertEquals($this->test_year,$this->guessYear->getYear());
+    }
+
+    function testGetWeek() {
+        $this->assertEquals($this->test_week, $this->guessYear->getWeek());
+    }
+
+    function testGetDateForMondayForWeek()
     {
-
+        $this->assertEquals("2015-12-28",$this->guessYear->getDateForMondayForWeek());
     }
+
+    function testGetYearForMondayForWeek()
+    {
+        $this->assertEquals("2015",$this->guessYear->getYearForMondayForWeek());
+    }
+    function testGetWeekForMondayForWeek()
+    {
+        $this->assertEquals(53,$this->guessYear->getWeekForMondayForWeek());
+    }
+
 }
